80ddbf335f67ec8325c5abfdd2c213fed6053583
Abdullah Gillani <72945262+agblaze@users.noreply.github.com>
2025-03-06 22:34:11 +0500

    Create setup_db.sql

---
+++ database/setup_db.sql
@@ -0,0 +1,10 @@
+CREATE TABLE inventory_data (
+    Date TEXT,
+    Inventory_Level INTEGER,
+    Past_Demand INTEGER,
+    Future_Demand INTEGER
+);
+
+INSERT INTO inventory_data VALUES
+('2025-01-01', 500, 450, 470),
+('2025-01-02', 480, 460, 480);
